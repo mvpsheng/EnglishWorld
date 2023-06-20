cccf8a672f0f4bc53e8690de1d29b480c161c458
mvpsheng <51054870+mvpsheng@users.noreply.github.com>
2023-06-20 07:03:55 +0000

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,22 @@ import './App.css'
 export default function App() {
   return (
     <main>
+      <header>
+        <div>
+          Logo
+        </div>
+        <div>
+          SearchBar
+        </div>
+        <navs>
+          <a>Words</a>
+          <a>Listening</a>
+          <a>Writing</a>
+          <a>Reading</a>
+          <a>Speaking</a>
+          <a>Subjects</a>
+        </navs>
+      </header>
       <h1>Hello World</h1>
     </main>
   )
